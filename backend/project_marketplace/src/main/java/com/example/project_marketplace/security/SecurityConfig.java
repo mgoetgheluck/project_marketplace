6e1afca08c814e0b2a9325546ad896b01ef5d43b
Maxime <mgoetghe@gmail.com>
2024-10-16 13:26:48 +0200

    adding security changes in order not to have to login at start

---
+++ backend/project_marketplace/src/main/java/com/example/project_marketplace/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.example.project_marketplace.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(expressionInterceptUrlRegistry ->
+                        expressionInterceptUrlRegistry
+                                .anyRequest()
+                                .permitAll());
+        return http.build();
+    }
+}
